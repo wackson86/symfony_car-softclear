21527e4fa4255eb26df55336579e94070640df2c
wackson86 <wackson86.jm@gmail.com>
2018-08-09 17:55:52 -0400

    Modificación generada para mostrar datos extraidos de la bd

--- src/Tienda/VentaBundle/Controller/DefaultController.php
+++ src/Tienda/VentaBundle/Controller/DefaultController.php
@@ -3,11 +3,15 @@
 namespace Tienda\VentaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Tienda\VentaBundle\Entity\Products;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('VentaBundle:Default:index.html.twig');
+        $em = $this->getDoctrine()->getEntityManager();
+        $productos = $em->getRepository('Tienda:Products')->findAll();
+
+        return $this->render('VentaBundle:Default:index.html.twig', array('productos' => $productos));
     }
 }
